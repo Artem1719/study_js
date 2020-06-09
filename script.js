d301b0a59c0bfff89e357ef3612bd533cee3428c
Artem Nikolaev <artem.nicolaev@gmail.com>
2020-06-10 02:38:14 +0300

    Первый коммит

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log(3);
+alert("Hello world!");
+console.log(1);
+var myVar = 10;
+
+
+let money = 150;
+const income = 5;
+let addExpenses = 33;
+const deposit = 1;
+let mission = "slovo";
+let period = true;
+console.log(money);
+console.log(income);
+console.log(period);
+console.log(mission);
