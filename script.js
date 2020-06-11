67c0f9072c3a71ef3acc122dc374a24a58095d23
Artem Nikolaev <artem.nicolaev@gmail.com>
2020-06-11 23:38:46 +0300

    Сделал второй урок

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-console.log(3);
-alert("Hello world!");
-console.log(1);
-
-
-
-let money = 1500;
-const income = 5;
-let addExpenses = 33;
-const deposit = 1;
-let mission = "slovo";
-let period = true;
-console.log(money);
-console.log(income);
-console.log(period);
-console.log(mission);
-console.log(deposit);
+let money = 30000;
+console.log(typeof money);
+let income = 'frilance';
+console.log(typeof income);
+let addExpenses = 'Internet, Mobile connection, Communal expenses';
+let deposit = true;
+console.log(typeof deposit);
+let mission = 200000;
+let period = 12;
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+console.log(addExpenses.toLowerCase().split(', '));
+let budgetDay = money/30;
+console.log('budgetDay: ', budgetDay);
