3d51594c934d4469cfc0de9b22ce7f3525ad249e
Artem Nikolaev <artem.nicolaev@gmail.com>
2020-06-12 19:48:27 +0300

    Добавил данные со второго урока

--- script.js
+++ script.js
@@ -4,6 +4,18 @@
 //console.log('question: ', question);
 //console.log(typeof question2);
 
-let n='10'
-n*=1
-console.log(typeof n);
+let money = 30000;
+console.log(typeof money);
+let income = 'frilance';
+console.log(typeof income);
+let addExpenses = 'Internet, Mobile connection, Communal expenses';
+let deposit = true;
+console.log(typeof deposit);
+let mission = 200000;
+let period = 12;
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+console.log(addExpenses.toLowerCase().split(', '));
+let budgetDay = money/30;
+console.log('budgetDay: ', budgetDay); 
