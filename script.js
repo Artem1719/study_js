7214dc15f9e13bed29417968ac113ac0686e80a1
Artem Nikolaev <artem.nicolaev@gmail.com>
2020-06-12 16:25:04 +0300

    Закомментарил переменные

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-let question=confirm('Тебе есть 18 лет?');
-let question2=prompt('Сколько тебе лет?');
-console.log('question: ', question);
-console.log('question2: ', question2);
+//let question=confirm('Тебе есть 18 лет?');
+//let question2=prompt('Сколько тебе лет?', '18');
+//console.log('question: ', question);
+//console.log('question2: ', question2);
