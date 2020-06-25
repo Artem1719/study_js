984cfdb6738a7aa2252116e461154fb962a1ad64
Artem Nikolaev <artem.nicolaev@gmail.com>
2020-06-25 20:34:44 +0300

    Перекопировал в 8 урок

--- script.js
+++ script.js
@@ -65,16 +65,11 @@ let appData = {
     }
 };
 appData.asking();
-//console.log('appData: ', appData);
-
-//console.log('Возможные расходы: ', appData.addExpenses);
-//console.log(appData.expenses);
 
 appData.getExpensesMonth();
 console.log('Обязательные расходы за месяц ', + appData.expensesMonth);
 
 appData.budgetMonth = appData.getBudget();
-//console.log('Месячный бюджет равен', appData.budgetMonth);
 
 if (appData.getTargetMonth() > 0) {
     console.log('Цель будет достигнута за ' + appData.getTargetMonth() + ' месяцев');
@@ -82,7 +77,6 @@ if (appData.getTargetMonth() > 0) {
 else console.log('Цель не будет достигнута');
 
 appData.budgetDay = Math.floor(appData.budgetMonth/30);
-//console.log('Бюджет на день:', appData.budgetDay);
 
 console.log(appData.getStatusIncome());
 
