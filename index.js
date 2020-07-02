8df1b1ba4740f9fbab36332a594f6c3db07fe52d
Artem Nikolaev <artem.nicolaev@gmail.com>
2020-07-02 23:16:52 +0300

    половина задания

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 
 const start = document.getElementById('start');
 console.log(start);
+const btnPlus1 = document.querySelectorAll('button')[0];
+console.log(btnPlus1);
+const btnPlus2 = document.querySelectorAll('button')[1];
+console.log(btnPlus2);
+const depositCheck = document.querySelector('#deposit-check');
+console.log(depositCheck);
+const additionalIncomeItem1 = document.querySelectorAll('.additional_income-item')[0];
+console.log(additionalIncomeItem1);
+const additionalIncomeItem2 = document.querySelectorAll('.additional_income-item')[1];
+console.log(additionalIncomeItem2);
